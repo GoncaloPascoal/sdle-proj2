852423d65b65063dcdf089bdfd29c3b16f9d9192
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-01-03 22:30:07 +0000

    Remaining command line arguments and bootstrapping

--- src/node.py
+++ src/node.py
@@ -1,8 +1,30 @@
 
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
+import asyncio
+
+from argparse import ArgumentParser, ArgumentTypeError, RawDescriptionHelpFormatter
 from utils import alnum
 
-def main():
+from kademlia.network import Server
+
+def parse_address(addr):
+    parts = addr.split(':')
+
+    if len(parts) < 2:
+        raise ArgumentTypeError('Address must be in ip:port format')
+    
+    ip = parts[0]
+
+    if ip == '':
+        ip = '127.0.0.1'
+    
+    try:
+        port = int(parts[1])
+    except:
+        raise ArgumentTypeError('Port must be an integer')
+    
+    return ip, port
+
+async def main():
     parser = ArgumentParser(description='Node that is part of a decentralized '
         'timeline newtwork.\nIt publishes small text messages (posts) to its '
         'timeline and can locate other nodes using a DHT algorithm and '
@@ -14,8 +36,18 @@ def main():
     parser.add_argument('id', help='alphanumeric node identifier', type=alnum)
     parser.add_argument('rpc_port', help='port used for remote procedure call',
         type=int)
+    parser.add_argument('port', help='port used for Kademlia DHT', type=int)
+    parser.add_argument('peers', help='ip:port pairs to use in the bootstrapping process',
+        metavar='PEER', nargs='+', type=parse_address)
 
     args = parser.parse_args()
 
+    node = Server()
+    await node.listen(args.port)
+
+    # Start the bootstrapping process (providing addresses for more nodes in 
+    # the command line arguments gives more fault tolerance)
+    await node.bootstrap(args.peers)
+
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
