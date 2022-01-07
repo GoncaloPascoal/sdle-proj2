bddaafab0da2947a12f1a9c3abccf3763e612523
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-01-07 15:44:47 +0000

    Implement SUBSCRIBE and GET procedures
    GET is still missing fault tolerance

--- src/node.py
+++ src/node.py
@@ -11,18 +11,22 @@ from kademlia.network import Server
 from kademlia.utils import digest
 
 class Post:
+    counter = 0
+
     def __init__(self, message: str):
+        self.id = Post.counter
+        Post.counter += 1
         self.timestamp = time.time_ns()
         self.message = message
-    
+
     def __repr__(self) -> str:
         dt = datetime.fromtimestamp(self.timestamp / 1e9)
-        return f'({dt} - {self.message})'
+        return f'({self.id} - {dt} - {self.message})'
 
 def parse_address(addr: str) -> Tuple[str, int]:
     parts = addr.split(':')
 
-    if len(parts) < 2:
+    if len(parts) != 2:
         raise ArgumentTypeError('Address must be in ip:port format')
 
     ip = parts[0]
@@ -44,17 +48,47 @@ async def post(message: str) -> ByteString:
     posts.append(Post(message))
     return b'OK'
 
-async def subscribe(node: Server, id_self: str, id: str) -> ByteString:
+async def subscribe(context: zmq.Context, node: Server, id_self: str, id: str) -> ByteString:
     global subscriptions
 
     if id == id_self:
         return b'Error: a peer cannot subscribe to itself'
 
-    subscriptions.add(id)
-    print(f'SUB: {id}')
+    info = await node.get(id)
+    if info:
+        info = json.loads(info)
+        port = info['port']
+
+        sock = context.socket(zmq.REQ)
+        sock.connect(f'tcp://127.0.0.1:{port}') # TODO: IP
+
+        sock.send_json({
+            'method': 'SUB_NODE',
+            'id': id_self,
+        })
+
+        res = sock.recv_string()
+        if res == 'OK':
+            print(f'SUB: {id}')
+            subscriptions.add(id)
+
+        return res.encode()
+
+    return b'Error: subscription process failed'
+
+async def subscribe_node(node: Server, args: Namespace, id: str) -> ByteString:
+    global subscribers
+    
+    if id in subscribers:
+        return b'Error: this node is already subscribed'
+
+    subscribers.add(id)
+    await update_kademlia_info(node, args)
+
+    print(f'SUB_NODE: {id}')
     return b'OK'
 
-async def get_timeline(node: Server, id_self: str, id: str) -> ByteString:
+async def get(context: zmq.Context, node: Server, id_self: str, id: str) -> ByteString:
     global subscriptions
 
     if id == id_self:
@@ -64,8 +98,40 @@ async def get_timeline(node: Server, id_self: str, id: str) -> ByteString:
     if id not in subscriptions:
         return b'Error: this peer is not subscribed to this id'
 
+    info = await node.get(id)
+    if info:
+        info = json.loads(info)
+        port = info['port']
+
+        sock = context.socket(zmq.REQ) # TODO: Abort if connection fails
+        sock.connect(f'tcp://127.0.0.1:{port}') # TODO: IP
+
+        sock.send_json({
+            'method': 'GET_NODE',
+        })
+
+        res = json.loads(sock.recv_string())
+        print(res)
+
+        return b'OK'
+
     print(f'Searching for timeline: {id}')
     return b'OK'
+
+async def get_node() -> ByteString:
+    global posts
+
+    posts_str = list(map(str, posts))
+    return json.dumps(posts_str).encode()
+
+async def update_kademlia_info(node: Server, args: Namespace):
+    global subscribers
+
+    info = gen_kademlia_info(args.rpc_port)
+    await node.set(args.id, info)
+    node.storage[digest(args.id)] = info
+
+    print('Updated Kademlia information')
 
 posts = []
 subscriptions = set()
@@ -81,13 +147,17 @@ def handle_requests(node: Server, args: Namespace):
     rpc_commands = {
         'POST': post,
         'SUB': subscribe,
-        'GET': get_timeline,
+        'SUB_NODE': subscribe_node,
+        'GET': get,
+        'GET_NODE': get_node,
     }
 
     rpc_args = {
         'POST': [],
-        'SUB': [('node', node), ('id_self', args.id)],
-        'GET': [('node', node), ('id_self', args.id)],
+        'SUB': [('context', context), ('node', node), ('id_self', args.id)],
+        'SUB_NODE': [('node', node), ('args', args)],
+        'GET': [('context', context), ('node', node), ('id_self', args.id)],
+        'GET_NODE': [],
     }
 
     print(f'Node {args.id} online...')
@@ -109,6 +179,16 @@ def handle_requests(node: Server, args: Namespace):
             parts[2] = b'Error: malformed command'
 
         sock.send_multipart(parts)
+
+def gen_kademlia_info(port: int) -> str:
+    global subscribers
+
+    info = {
+        'port': port,
+        'subscribers': list(subscribers),
+    }
+
+    return json.dumps(info)
 
 def main():
     parser = ArgumentParser(description='Node that is part of a decentralized '
@@ -151,7 +231,9 @@ def main():
         print('Bootstrap process finished...')
     else:
         print('Starting a new Kademlia network...')
-    
+
+    loop.run_until_complete(update_kademlia_info(node, args))
+
     server_thread = Thread(target=handle_requests, args=(node, args))
     server_thread.start()
 
